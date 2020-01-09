c08028b5816205d36d6dd6fad2a9dd80633a126b
hm <hm.dev7@gmail.com>
2020-01-09 10:12:46 +0700

    #14 Avoiding recursive logins

--- src/main/kotlin/id/mustofa/atm/command/LoginCommand.kt
+++ src/main/kotlin/id/mustofa/atm/command/LoginCommand.kt
@@ -6,15 +6,22 @@ import id.mustofa.atm.command.base.SingleArgCommand
 import id.mustofa.atm.router.UserCommandsRouter
 import id.mustofa.atm.util.Database
 import id.mustofa.atm.util.enterNestedCommandSet
+import id.mustofa.atm.util.handled
+import java.util.*
 import javax.inject.Inject
 
 class LoginCommand @Inject constructor(
     private val database: Database,
     private val outputter: Outputter,
-    private val userCommandsRouterFactory: UserCommandsRouter.Factory
+    private val userCommandsRouterFactory: UserCommandsRouter.Factory,
+    private val account: Optional<Database.Account>
 ) : SingleArgCommand() {
 
     override fun handleArg(arg: String): Result {
+        if (account.isPresent) {
+            outputter.output("Can't login! \"${account.get().username}\" already logged in")
+            return Result.handled
+        }
         val account = database.getAccount(arg)
         outputter.output("${account.username} is logged in with balance: ${account.balance}.")
         return Result.enterNestedCommandSet(userCommandsRouterFactory.create(account).router())
--- src/main/kotlin/id/mustofa/atm/module/LoginCommandModule.kt
+++ src/main/kotlin/id/mustofa/atm/module/LoginCommandModule.kt
@@ -1,11 +1,13 @@
 package id.mustofa.atm.module
 
 import dagger.Binds
+import dagger.BindsOptionalOf
 import dagger.Module
 import dagger.multibindings.IntoMap
 import dagger.multibindings.StringKey
 import id.mustofa.atm.command.LoginCommand
 import id.mustofa.atm.command.base.Command
+import id.mustofa.atm.util.Database
 
 @Module
 abstract class LoginCommandModule {
@@ -14,4 +16,7 @@ abstract class LoginCommandModule {
     @IntoMap
     @StringKey("login")
     abstract fun loginCommand(command: LoginCommand): Command
+
+    @BindsOptionalOf
+    abstract fun optionalAccount(): Database.Account
 }
