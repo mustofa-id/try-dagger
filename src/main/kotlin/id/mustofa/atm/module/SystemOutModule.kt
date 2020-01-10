ece8535496a1a6a8d5f122256c647c99e31a7b6f
hm <hm.dev7@gmail.com>
2020-01-10 08:48:25 +0700

    Using static members

--- src/main/kotlin/id/mustofa/atm/module/SystemOutModule.kt
+++ src/main/kotlin/id/mustofa/atm/module/SystemOutModule.kt
@@ -6,12 +6,13 @@ import id.mustofa.atm.command.base.Outputter
 import id.mustofa.atm.util.TextOutputter
 
 @Module
-class SystemOutModule {
+object SystemOutModule {
 
     // @Provides methods are concrete methods in a module that tell Dagger that
     // when something requests an instance of the type the method returns,
     // it should call that method to get an instance. Like @Inject constructors,
     // they can have parameters: those parameters are their dependencies.
+    @JvmStatic
     @Provides
     fun textOutputter(): Outputter {
         return TextOutputter()
